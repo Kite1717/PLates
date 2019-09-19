998b110370d4fb56e44827b806d509a054fcf622
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2019-09-20 01:45:38 +0300

    Create Menu

--- HM.c
+++ HM.c
@@ -50,4 +50,48 @@ main()
 {
 	//plates array
     plates *plates = (plateInfo*)calloc(82,(sizeof(plateInfo))); // between 1 and 81
+    
+     while(1)
+	{
+		char *temp[100];
+		printf("\nChoose option :\n1 - Add Plate\n2 - List of all plates by cities\n3 - Listing by plate code\n4 - Clean Screen\n0 - Exit\n");
+		scanf("%s",temp);
+	    if(checkDigit(temp))
+		{
+		  int choice = atoi(temp);
+		switch(choice)
+		{
+			case 1 : {
+				plateInfo p = input();
+				if(addPlate(plates,p))
+				 printf("\nSuccesful insertion :)\n");
+				else
+				 printf("\nFails to Add :(\n");
+				break;
+			}
+			case 2:{
+				display(plates);
+				break;
+			}
+			case 3 :{
+				listingByPlateCode(plates);
+				break;
+			}
+			case 4:{
+				system("cls");
+				break;
+			}
+			case 0:{		
+				return 0;
+			}
+			default:{
+					printf("\nInvalid value\n");
+				break;
+			}			
+		}
+	}
+	else{
+		printf("\nInvalid value\n");
+	}
+	}
 }
